a270e11d6109ccfb7d424fc1ff44ced11dc5bf65
kdeng <kdeng@hillinsight.com>
2018-07-04 18:14:20 +0800

    提交首页代码

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
+var fs = require('fs');
+var path = require('path');
 var express = require('express');
 var router = express.Router();
 
+var homePageExists = fs.existsSync(path.join(__dirname, '../public/homepage/index.html'));
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    if (homePageExists) {
+        res.redirect('/homepage/index.html');
+    } else {
+        next();
+    }
 });
 
 module.exports = router;

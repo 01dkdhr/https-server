8c3aef461d3bf8d7305ac6b66cdfd5fc53bd184f
kdeng <kdeng@hillinsight.com>
2018-07-05 10:58:46 +0800

    修改首页路劲

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,7 @@
-var fs = require('fs');
-var path = require('path');
 var express = require('express');
 var router = express.Router();
 
-var homePageExists = fs.existsSync(path.join(__dirname, '../public/homepage/index.html'));
-
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    if (homePageExists) {
-        res.redirect('/homepage/index.html');
-    } else {
-        next();
-    }
-});
+// default get index.html
 
 module.exports = router;

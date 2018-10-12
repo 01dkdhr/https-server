429c9566b696c71c2ece9c6b42a7ca7a01e47c81
kdeng <369069851@hillinsight.com>
2018-10-12 17:11:25 +0800

    提交 上传微服务

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,24 @@
+var fs = require('fs');
+var path = require('path');
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-// default get index.html
+// 访问微服务
+(function routeMicroServes() {
+    try {
+        var filePath = path.join(__dirname, '../micro_serves');
+        var files = fs.readdirSync(filePath);
+        files.forEach((file) => {
+            var serve = require(path.join(filePath, file));
+            if (serve && serve.onRequest) {
+                router.get(`/micro-serves/${file}`, function(req, res, next) {
+                    serve.onRequest(req, res);
+                });
+            }
+        })
+    } catch (err) {
+        console.log('routeMicroServes err', err);
+    }
+}());
 
 module.exports = router;

4a3383a44f3740cf2fdc2fe388d27a97556a243d
kdeng <369069851@hillinsight.com>
2018-10-18 18:07:32 +0800

    123

--- api/stock/index.js
+++ api/stock/index.js
@@ -25,7 +25,7 @@ module.exports = {
             .then((client)=> {
                 const db = client.db('node-stock');
                 const collection = db.collection('stocks');
-                collection.find({}).toArray((err, result) => {
+                collection.find({}, {"_id": 0}).toArray((err, result) => {
                     if (err) {
                         console.log('/stock-list err:', err);
                         responseErr(res, 10002);

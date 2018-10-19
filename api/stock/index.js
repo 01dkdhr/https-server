d49f9858ce5b389e0970c072543eaa44ab0eaf56
kdeng <369069851@hillinsight.com>
2018-10-19 17:22:01 +0800

    提交新的api

--- api/stock/index.js
+++ api/stock/index.js
@@ -20,28 +20,79 @@ function responseSuccess(res, result) {
 
 module.exports = {
     onRequest(req, res) {
-        if (req.url == '/stock-list') {
-            MongoUtil.getClient('node-stock')
-            .then((client)=> {
-                const db = client.db('node-stock');
-                const collection = db.collection('stocks');
-                collection.find({}, { projection: {"_id": 0} }).toArray((err, result) => {
-                    if (err) {
-                        console.log('/stock-list err:', err);
-                        responseErr(res, 10002);
-                    } else {
-                        responseSuccess(res, JSON.stringify(result));
-                    }
-
-                    MongoUtil.disConnect(client);
+        try {
+            if (req.path == '/stock-list') {
+                MongoUtil.getClient('node-stock')
+                .then((client)=> {
+                    const db = client.db('node-stock');
+                    const collection = db.collection('stocks');
+                    collection.find({}, { projection: {"_id": 0, "price_tick": 0, "delisted_data": 0, "sec_type": 0} }).toArray((err, result) => {
+                        if (err) {
+                            console.log('/stock-list err:', err);
+                            responseErr(res, 10002);
+                        } else {
+                            responseSuccess(res, JSON.stringify(result));
+                        }
+    
+                        MongoUtil.disConnect(client);
+                    });
+                })
+                .catch((err) => {
+                    console.log('/stock-list err:', err);
+                    responseErr(res, 10001);
                 });
-            })
-            .catch((err) => {
-                console.log('/stock-list err:', err);
-                responseErr(res, 10001);
-            });
-        } else {
-            responseErr(res, 10000);
+            } else if (req.path == '/daily-stock') {
+                const symbol = req.query && req.query.symbol || '';
+                if (!symbol) {
+                    responseErr(res, 10001);   
+                    return; 
+                }
+                MongoUtil.getClient('node-stock')
+                .then((client)=> {
+                    const db = client.db('node-stock');
+
+                    // 从stocks里查询stock信息
+                    const p1 = new Promise((resolve, reject) => {
+                        db.collection('stocks').findOne({"symbol": symbol}, (err, result) => {
+                            if (err || !result) {
+                                reject(err);
+                                return;
+                            }
+
+                            resolve(result);
+                        });
+                    });
+
+                    // 从daily-stocks里查询stock信息
+                    const p2 = new Promise((resolve, reject) => {
+                        db.collection('daily-stocks').findOne({"symbol": symbol}, (err, result) => {
+                            if (err || !result || !result.data) {
+                                reject(err);
+                                return;
+                            }
+
+                            resolve(result.data);
+                        });
+                    });
+
+                    return Promise.all([p1, p2]);
+                })
+                .then((result) => {
+                    responseSuccess(res, JSON.stringify({
+                        info: result[0],
+                        data: result[1]
+                    }));    
+                })
+                .catch((err) => {
+                    console.log('/stock-list err:', err);
+                    responseErr(res, 10001);
+                });
+            } else {
+                responseErr(res, 10000);
+            }
+        } catch (err) {
+            console.log(`${req.url} err: ${err}`);
+            responseErr(res, 20000); 
         }
     }
 };

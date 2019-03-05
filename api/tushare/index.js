f1a496baf94f019a6f2fcea7009ee9a74b917700
kdeng <369069851@hillinsight.com>
2019-03-05 17:50:28 +0800

    123

--- api/tushare/index.js
+++ api/tushare/index.js
@@ -12,11 +12,12 @@ function responseErr(res, errCode, msg) {
 }
 
 function responseSuccess(res, result) {
-  res.writeHead(200, { 
-    'content-type': 'application/json; charset=UTF-8', 
-    "Access-Control-Allow-Origin": "*", 
-    'transfer-encoding': 'chunked', 
-    'vary': 'Accept-Encoding' 
+  res.writeHead(200, {
+    'content-type': 'application/json; charset=UTF-8',
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept",
+    'transfer-encoding': 'chunked',
+    'vary': 'Accept-Encoding'
   });
   res.end(JSON.stringify({
     err_code: 0,
@@ -36,23 +37,24 @@ function requestPromise(option) {
 module.exports = {
   async onRequest(req, res) {
     try {
-      const query = _.cloneDeep(req.body);
-      if (query.params) {
-        query.params = JSON.parse(query.params)
-      }
-      if (query.api_name) {
-        const body = await requestPromise({
-          method: 'POST',
-          url: 'http://api.tushare.pro',
-          headers: { "Content-Type": "application/x-www-form-urlencoded" },
-          form: JSON.stringify(Object.assign(query, { token:  localConfig["tushare-token"]}))
-        });
+      if (req.method == 'POST') {
+        const query = _.cloneDeep(req.body);
+        if (query.api_name) {
+          const body = await requestPromise({
+            method: 'POST',
+            url: 'http://api.tushare.pro',
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            form: JSON.stringify(Object.assign(query, { token: localConfig["tushare-token"] }))
+          });
 
-        responseSuccess(res, body);
-      } else {
-        responseErr(res, 10000);
+          responseSuccess(res, body);
+        } else {
+          responseErr(res, 10000);
+        }
+      } else if (req.method == 'OPTIONS') {
+        responseSuccess(res, 200);  
       }
-    } catch(err) {
+    } catch (err) {
       console.log(`${req.url} err: ${err}`);
       responseErr(res, 20000);
     }
